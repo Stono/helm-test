357e23edbaef2d60d88adabb5b2e17b660fd8a0d
Karl Stoney <me@karlstoney.com>
2018-07-15 16:54:44 +0100

    Revert "Running test against all charts & helm lint"
    This reverts commit a938213693f05036abf729229e6afc3f7e3efd37.

--- test/helm.js
+++ test/helm.js
@@ -7,33 +7,12 @@ describe('Helm', () => {
   beforeEach(() => {
     exec = deride.stub(['command']);
     exec.setup.command.toCallbackWith([null, { stdout: '' }]);
+    helm = new Helm(exec);
   });
-  describe('Specific charts', () => {
-    beforeEach(() => {
-      helm = new Helm(exec, '.');
-    });
-    it('should accept value files', () => {
-      helm.withValueFile('some-file.yaml');
-    });
-    it('should run a helm template', done => {
-      helm.go(done);
-    });
-    it('should run a helm lint', done => {
-      helm.lint(done);
-    });
+  it('should accept value files', () => {
+    helm.withValueFile('some-file.yaml');
   });
-  describe('All charts', () => {
-    beforeEach(() => {
-      helm = new Helm(exec);
-    });
-    it('should accept value files', () => {
-      helm.withValueFile('chart-name/some-file.yaml');
-    });
-    it('should run a helm template', done => {
-      helm.go(done, 'chart-name');
-    });
-    it('should run a helm lint', done => {
-      helm.lint(done, 'chart-name');
-    });
+  it('should run a helm template', done => {
+    helm.go(done);
   });
 });
